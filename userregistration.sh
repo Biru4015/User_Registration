fcb99f7fbe22b4e92bb0223b8f9791aed3b25613
Biru4015 <rambirendra12@gmail.com>
2020-03-27 19:44:45 +0530

    [Birendra] Adding regex for last name in user registration

--- userregistration.sh
+++ userregistration.sh
@@ -12,4 +12,17 @@ firstName()
          echo "First name is not valid"
      fi
 }
+lastName()
+{
+   echo -e "Enter your last name:\c"
+   read lastname
+   lname="^([A-Z])([a-z]{2,})$"
+     if [[ $lastname =~ $lname ]]
+     then
+         echo "Last name is valid."
+     else
+         echo "Last name is not valid"
+     fi
+}
 firstName
+lastName
